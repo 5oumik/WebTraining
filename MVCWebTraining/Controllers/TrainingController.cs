72e8197b17ef0bef2b49c977c93651a2f7dbf7f0
SoumikBag <soumik.bag@northgate-is.com>
2017-10-18 17:30:28 +0530

    MVCWebTraining

---
+++ MVCWebTraining/Controllers/TrainingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebTraining.Controllers
+{
+    public class TrainingController : Controller
+    {
+        // GET: Training
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        public string Welcome()
+        {
+            return "This is Welcome action method...";
+        }
+    }
+}
---
+++ MVCWebTraining/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCWebTraining.Global" Language="C#" %>
